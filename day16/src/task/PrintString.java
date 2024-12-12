3cf86f0e21d04fd10adc8c3e0ac13be2f19def67
mo0n0902 <1answnsgur1@gmail.com>
2024-12-13 00:01:09 +0900

    1212 과제

---
+++ day16/src/task/PrintString.java
@@ -0,0 +1,27 @@
+package task;
+
+public class PrintString {
+
+	// 필드
+	String str1;
+
+	//생성자
+	public PrintString(String str1) {
+		super();
+		this.str1 = str1;
+	}
+	
+	//메소드
+	void printStrInt(String str1) {
+		try {
+			int num = Integer.parseInt(str1); //드래그해서 try catch 생성
+			System.out.println("입력한 문자열 : " + str1 + "변환한 정수 : " + num);
+		} catch (NumberFormatException e) {
+			System.out.println("숫자가 아닌 문자열이 입력되었습니다"); 
+			
+		}
+		
+	}
+	
+	
+}
---
+++ day16/src/task/PrintStringMain.java
@@ -0,0 +1,16 @@
+package task;
+
+import java.util.Scanner;
+
+public class PrintStringMain {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("문자열을 입력해주세요 : ");
+	String str1 = sc.next();
+	
+	PrintString ps = new PrintString(str1);
+	ps.printStrInt(str1);
+	
+}
+}
